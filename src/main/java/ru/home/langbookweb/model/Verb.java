45fdea59a6371a79a9f29268baea979b44491436
Iogann1978 <iogann1978@gmail.com>
2021-04-27 15:14:15 +0300

    add function prepending s for verbs

--- src/main/java/ru/home/langbookweb/model/Verb.java
+++ src/main/java/ru/home/langbookweb/model/Verb.java
@@ -19,4 +19,19 @@ public class Verb extends Word {
     @NotNull
     @Column(nullable = false)
     private String participle;
+
+    public static String getEndingS(String verb) {
+        if (verb == null)
+            return null;
+        if ("have".equals(verb))
+            return "has";
+        if ("am".equals(verb))
+            return "is";
+        if (verb.endsWith("o") || verb.endsWith("x") || verb.endsWith("sh") || verb.endsWith("ch") || verb.endsWith("tch")
+                || verb.endsWith("zz") || verb.endsWith("s"))
+            return verb + "es";
+        if (verb.endsWith("y"))
+            return verb.substring(0, verb.length() - 1) + "ies";
+        return verb + "s";
+    }
 }
--- src/test/java/ru/home/langbookweb/LangbookwebApplicationTests.java
+++ src/test/java/ru/home/langbookweb/LangbookwebApplicationTests.java
@@ -2,13 +2,56 @@ package ru.home.langbookweb;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import ru.home.langbookweb.model.User;
+import ru.home.langbookweb.model.Verb;
+import ru.home.langbookweb.model.WordItem;
+import ru.home.langbookweb.repository.UserRepository;
+import ru.home.langbookweb.repository.WordItemRepository;
+import ru.home.langbookweb.repository.WordRepository;
+
+import java.util.Iterator;
 
 @SpringBootTest
 @Slf4j
 class LangbookwebApplicationTests {
+    @Autowired
+    private WordRepository wordRepository;
+    @Autowired
+    private WordItemRepository wordItemRepository;
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
     public void testProcessWord() {
         log.info("word: {}", AbstractLoadService.processWord("a \"the word\" "));
+    }
+
+    @Test
+    public void recalcText() {
+        User user = userRepository.findById("user").get();
+        long count = wordItemRepository.countAllByUser(user);
+        Page<WordItem> page = wordItemRepository.getAllByUser(user, PageRequest.of(0, (int)count));
+        Iterator<WordItem> iter = page.iterator();
+        while (iter.hasNext()) {
+            WordItem wi = iter.next();
+            if (wordRepository.existsWordByUserAndWord(user, wi.getWord())) {
+                log.info("del wordItem: {}", wi.getWord());
+                wordItemRepository.delete(wi);
+            }
+        }
+    }
+
+    //@Test
+    public void testVerbEndingS() {
+        Verb v1 = Verb.builder().word("dry").build();
+        log.info("verb: {} {}", v1.getWord(), Verb.getEndingS(v1.getWord()));
+        Verb v2 = Verb.builder().word("do").build();
+        log.info("verb: {} {}", v2.getWord(), Verb.getEndingS(v2.getWord()));
+        Verb v3 = Verb.builder().word("kiss").build();
+        log.info("verb: {} {}", v3.getWord(), Verb.getEndingS(v3.getWord()));
     }
 }
