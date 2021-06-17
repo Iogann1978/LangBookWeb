eb727e1f0868cc9e74d2a081af7a09d5c1dcade9
Iogann1978 <iogann1978@gmail.com>
2021-06-17 17:26:48 +0300

    add words

--- src/main/java/ru/home/langbookweb/controller/TextController.java
+++ src/main/java/ru/home/langbookweb/controller/TextController.java
@@ -2,7 +2,10 @@ package ru.home.langbookweb.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
 import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.data.domain.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,6 +16,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.home.langbookweb.model.WordItem;
 import ru.home.langbookweb.service.TextService;
@@ -84,6 +88,13 @@ public class TextController extends AbstractPageController {
             }
             return options.equals("0") ? textService.parse(content) : textService.translate(content);
         }))).flatMap(count -> {
+            /*
+            DataBufferFactory dataBufferFactory = new DefaultDataBufferFactory();
+            response.setStatusCode(HttpStatus.OK);
+            response.getHeaders().setContentType(MediaType.TEXT_PLAIN);
+            DataBuffer buf = dataBufferFactory.wrap("Hello from exchange".getBytes(StandardCharsets.UTF_8));
+            return response.writeWith(Flux.just(buf));
+             */
             response.setStatusCode(HttpStatus.SEE_OTHER);
             response.getHeaders().setLocation(URI.create("/text"));
             return response.setComplete();
